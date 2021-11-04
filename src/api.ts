49221b3b04e2f24992a846754df10136dc974571
Julian Hecker <julianheckerdev@gmail.com>
2021-11-03 23:48:09 -0400

    setup stripe on backend

--- src/api.ts
+++ src/api.ts
@@ -1,13 +1,26 @@
-import express, { Request, Response } from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 export const app = express();
 
 import cors from 'cors';
+import { createStripeCheckoutSession } from './checkout';
 app.use(cors({ origin: true }));
 app.use(express.json());
 app.get('/', (req: Request, res: Response) => {
-  res.send('hey welcome')
-})
+  res.send('hey welcome');
+});
 app.post('/test', (req: Request, res: Response) => {
   const amount = req.body.amount;
   res.status(200).send({ with_tax: amount * 1.075 });
 });
+app.post(
+  '/checkouts/',
+  runAsync(async ({ body }: Request, res: Response) => {
+    res.send(await createStripeCheckoutSession(body.line_items));
+  }),
+);
+
+function runAsync(callback: Function) {
+  return (req: Request, res: Response, next: NextFunction) => {
+    callback(req, res, next).catch(next);
+  };
+}
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,15 @@
+import { config } from 'dotenv';
+if (process.env.NODE_ENV !== 'production') {
+  config();
+}
+
+import Stripe from 'stripe';
+export const stripe = new Stripe(process.env.STRIPE_SECRET ?? '', {
+  apiVersion: '2020-08-27',
+});
+
 import { app } from './api';
-const port = process.env.PORT || 3001;
+const port = process.env.PORT ?? 3001;
 app.listen(port, () =>
   console.log(`API started on http://localhost:${port}`),
 );
